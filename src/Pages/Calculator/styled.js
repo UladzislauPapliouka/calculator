041ac802ca4ef307f173b8387a3dcd8e4e2db40a
Uladzislau Papliouka <uladzislau.papliouka@gmail.com>
2023-03-09 09:55:15 +0300

    fix: change width of history column

--- src/Pages/Calculator/styled.js
+++ src/Pages/Calculator/styled.js
@@ -2,11 +2,11 @@ import styled from "styled-components";
 
 export const StyleCalculator =styled.div`
     display: grid;
-  grid-template-rows: 150px 1fr;
-  grid-template-columns:1fr 250px;
+    grid-template-rows: 150px 1fr;
+    grid-template-columns:1fr 400px;
     grid-template-areas: "disp  his"
                         "keyp  his";
     height: 89vh;
-  overflow: hidden;
-  position: relative;
+    overflow: hidden;
+    position: relative;
 `
