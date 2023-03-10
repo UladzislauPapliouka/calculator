1571df5f44b79306e8ec8b695f651f4fa019fe64
Uladzislau Papliouka <uladzislau.papliouka@gmail.com>
2023-03-10 09:42:15 +0300

    fix: create Operation and KeyPadTable constant

--- src/Components/Keypad/index.jsx
+++ src/Components/Keypad/index.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import {Key, KeypadWrapper} from "./styled";
-import {keys} from "../../Constants/KeypadConstansts";
+import {KeyPadTable} from "../../Constants/KeypadConstansts";
 
 const Keypad = ()=>{
     return(
         <KeypadWrapper>
-            {keys.map(key=><Key key={key}>{key}</Key>)}
+            {KeyPadTable.map(line=>line.map(key=><Key key={key}>{key}</Key>))}
         </KeypadWrapper>
     )
 }
