9b28b3c5a3e7f34de1c1b5d211aca5afe618d36b
Uladzislau Papliouka <uladzislau.papliouka@gmail.com>
2023-03-12 13:53:27 +0300

    fix: fix case division by zero

--- src/utils/ArithmeticUnit.js
+++ src/utils/ArithmeticUnit.js
@@ -10,7 +10,16 @@ export class ArithmeticUnit {
         return (value1 * value2).toFixed(3)
     }
     Divide = (value1, value2) => {
-        return (value1 / value2).toFixed(3)
+        try{
+            const result = (value1 / value2).toFixed(3)
+            console.log(result)
+            if(result === 'Infinity') {
+                throw new Error("Division by zero...")
+            }
+            return result
+        }catch (e){
+            return e
+        }
     }
     ReversSign = (value) => {
         return -value.toFixed(3)
