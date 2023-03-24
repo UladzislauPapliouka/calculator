3d3f790ee7198abeea64f440335a338b37ffc50b
Uladzislau Papliouka <uladzislau.papliouka@gmail.com>
2023-03-24 12:38:37 +0300

    fix: fix eslint

--- src/utils/calculator.js
+++ src/utils/calculator.js
@@ -8,12 +8,9 @@ export default class Calculator {
     const v1 = this.values.pop();
     const v2 = this.values.pop();
 
-    console.log(v1, v2);
-
     const value = command.execute(v1, v2);
 
     this.values.push(value);
-    console.log(this.values);
     this.history.push(value);
   }
 
